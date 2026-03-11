c4e40785677ed11cae764c4cc34f48904d6dd310
Pinery-lee <liyuanchao20@gmail.ucas.ac.cn>
2026-03-11 14:18:08 +0800

    基本结构

---
+++ data.js
@@ -0,0 +1,17 @@
+var default_mind = {
+    "meta": {
+        "name": "agri-rs-census4_Array",
+        "author": "liyuanchao",
+        "version": "0.1"
+    },
+    "format": "node_array",
+    "data": [
+        {
+            "id": "root",
+            "topic": "第四次全国农业普查遥感测量方案",
+            "expanded": true,
+            "isroot": true,
+            "background-image": ""
+        },
+    ]
+};
