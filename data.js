bd6dcbb9e96ed41603fcf31f1bc88696193b3c1a
橘子味的鱼 <84773395+Pinery-lee@users.noreply.github.com>
2026-03-12 09:37:59 +0800

    Update mind data.js: 2026/3/12 09:37:58

--- data.js
+++ data.js
@@ -11,7 +11,566 @@ var default_mind = {
             "topic": "第四次全国农业普查遥感测量方案",
             "expanded": true,
             "isroot": true,
-            "background-image": ""
+            "note": "通过卫星遥感、无人机、人工智能、实地调查等技术方法的综合应用，准确测量主要农作物播种面积，查清设施农业状况。"
         },
+        {
+            "id": "cdbb011a823b7757",
+            "topic": "①实地调查",
+            "expanded": true,
+            "parentid": "root",
+            "direction": "right",
+            "note": ""
+        },
+        {
+            "id": "cdbb410879b69cfe",
+            "topic": "农作物种植地块实地调查",
+            "expanded": true,
+            "parentid": "cdbb011a823b7757",
+            "note": ""
+        },
+        {
+            "id": "cdbb73bf717f2c73",
+            "topic": "确定实地调查样本",
+            "expanded": true,
+            "parentid": "cdbb410879b69cfe",
+            "note": ""
+        },
+        {
+            "id": "cdbb80a806dfc647",
+            "topic": "100个地块/县/作物；优先利旧，不足补充",
+            "expanded": true,
+            "parentid": "cdbb73bf717f2c73",
+            "note": ""
+        },
+        {
+            "id": "cdbb80c374252d58",
+            "topic": "播种面积 < 5 万亩可豁免补充调查",
+            "expanded": true,
+            "parentid": "cdbb73bf717f2c73",
+            "note": ""
+        },
+        {
+            "id": "cdbb75b50c9f6ac7",
+            "topic": "制作调查任务包",
+            "expanded": true,
+            "parentid": "cdbb410879b69cfe",
+            "note": ""
+        },
+        {
+            "id": "cdbba8aff0f8ee28",
+            "topic": "遥感影像",
+            "expanded": true,
+            "parentid": "cdbb75b50c9f6ac7",
+            "note": ""
+        },
+        {
+            "id": "cdbba8c67733e923",
+            "topic": "边界矢量",
+            "expanded": true,
+            "parentid": "cdbb75b50c9f6ac7",
+            "note": ""
+        },
+        {
+            "id": "cdbba8d2f2e7f560",
+            "topic": "调查样方矢量",
+            "expanded": true,
+            "parentid": "cdbb75b50c9f6ac7",
+            "note": ""
+        },
+        {
+            "id": "cdbba8e2fa04b2f2",
+            "topic": "自然地块矢量",
+            "expanded": true,
+            "parentid": "cdbb75b50c9f6ac7",
+            "note": ""
+        },
+        {
+            "id": "cdbb77cf76ca72f8",
+            "topic": "开展实地调查",
+            "expanded": true,
+            "parentid": "cdbb410879b69cfe",
+            "note": ""
+        },
+        {
+            "id": "cdbbdbc3fe568b11",
+            "topic": "采集工具：终端/无人机",
+            "expanded": true,
+            "parentid": "cdbb77cf76ca72f8",
+            "note": ""
+        },
+        {
+            "id": "cdbbdc046f5974b9",
+            "topic": "监测目标：季播作物种植属性与面积",
+            "expanded": true,
+            "parentid": "cdbb77cf76ca72f8",
+            "note": ""
+        },
+        {
+            "id": "cdbb412c80ead1ea",
+            "topic": "畜禽养殖设施实地调查",
+            "expanded": true,
+            "parentid": "cdbb011a823b7757",
+            "note": ""
+        },
+        {
+            "id": "cdbbfca1647466e1",
+            "topic": "确定实地调查样本",
+            "expanded": true,
+            "parentid": "cdbb412c80ead1ea",
+            "note": ""
+        },
+        {
+            "id": "cdbc01fef871db4e",
+            "topic": "标准: 切割点以上全量纳入",
+            "expanded": true,
+            "parentid": "cdbbfca1647466e1",
+            "note": ""
+        },
+        {
+            "id": "cdbc047e79509e93",
+            "topic": "注: 猪≥500头 / 省定标准 (可下调)",
+            "expanded": true,
+            "parentid": "cdbc01fef871db4e",
+            "note": ""
+        },
+        {
+            "id": "cdbbfcb3744bd28d",
+            "topic": "制作调查表",
+            "expanded": true,
+            "parentid": "cdbb412c80ead1ea",
+            "note": ""
+        },
+        {
+            "id": "cdbc1a51ef71e965",
+            "topic": "坐标采集 + 亚米级影像",
+            "expanded": true,
+            "parentid": "cdbbfcb3744bd28d",
+            "note": ""
+        },
+        {
+            "id": "cdbbfcbf6521412a",
+            "topic": "开展实地调查",
+            "expanded": true,
+            "parentid": "cdbb412c80ead1ea",
+            "note": ""
+        },
+        {
+            "id": "cdbc2104e9bd5007",
+            "topic": "逐场核实",
+            "expanded": true,
+            "parentid": "cdbbfcbf6521412a",
+            "note": ""
+        },
+        {
+            "id": "cdbc24ef7f7cfad6",
+            "topic": "三部曲: 确认、编码、信息登记",
+            "expanded": true,
+            "parentid": "cdbc2104e9bd5007",
+            "note": ""
+        },
+        {
+            "id": "cdbb2af8837bc761",
+            "topic": "②农作物种植用地遥感测量",
+            "expanded": true,
+            "parentid": "root",
+            "direction": "right",
+            "note": ""
+        },
+        {
+            "id": "cdbccde9515d3248",
+            "topic": "数据准备",
+            "expanded": true,
+            "parentid": "cdbb2af8837bc761",
+            "note": ""
+        },
+        {
+            "id": "cdbcd1b9ff2be18c",
+            "topic": "多源汇聚",
+            "expanded": true,
+            "parentid": "cdbccde9515d3248",
+            "note": ""
+        },
+        {
+            "id": "cdbcd3cd5bfb6c4d",
+            "topic": "影像、历史数据、实地数据、解译库、县界矢量",
+            "expanded": true,
+            "parentid": "cdbcd1b9ff2be18c",
+            "note": ""
+        },
+        {
+            "id": "cdbceaece5cf38b8",
+            "topic": "影像标准",
+            "expanded": true,
+            "parentid": "cdbccde9515d3248",
+            "note": ""
+        },
+        {
+            "id": "cdbccdf9e0134e01",
+            "topic": "基准生产",
+            "expanded": true,
+            "parentid": "cdbceaece5cf38b8",
+            "note": ""
+        },
+        {
+            "id": "cdbcdaa25596c1ee",
+            "topic": "上年度亚米级影像",
+            "expanded": true,
+            "parentid": "cdbccdf9e0134e01",
+            "note": ""
+        },
+        {
+            "id": "cdbcce0569d30736",
+            "topic": "动态更新",
+            "expanded": true,
+            "parentid": "cdbceaece5cf38b8",
+            "note": ""
+        },
+        {
+            "id": "cdbce082fee23829",
+            "topic": "当年优于2m(含)分辨率影像",
+            "expanded": true,
+            "parentid": "cdbcce0569d30736",
+            "note": ""
+        },
+        {
+            "id": "cdbcedef6e9332af",
+            "topic": "种植用地测量",
+            "expanded": true,
+            "parentid": "cdbb2af8837bc761",
+            "note": ""
+        },
+        {
+            "id": "cdbcf81a63e52272",
+            "topic": "既有地块更新/重绘",
+            "expanded": true,
+            "parentid": "cdbcedef6e9332af",
+            "note": ""
+        },
+        {
+            "id": "cdbcf835d7e1438b",
+            "topic": "空白区全量勾画",
+            "expanded": true,
+            "parentid": "cdbcedef6e9332af",
+            "note": ""
+        },
+        {
+            "id": "cdbcf84b506627c7",
+            "topic": "获取分县最新地块",
+            "expanded": true,
+            "parentid": "cdbcedef6e9332af",
+            "note": ""
+        },
+        {
+            "id": "cdbd098f6ae832ec",
+            "topic": "质量检查",
+            "expanded": true,
+            "parentid": "cdbb2af8837bc761",
+            "note": ""
+        },
+        {
+            "id": "cdbd0d52dc240f55",
+            "topic": "自检：生产单位随产随检",
+            "expanded": true,
+            "parentid": "cdbd098f6ae832ec",
+            "note": ""
+        },
+        {
+            "id": "cdbd0d71ec732e98",
+            "topic": "审核：第三方机构专项数据审计",
+            "expanded": true,
+            "parentid": "cdbd098f6ae832ec",
+            "note": ""
+        },
+        {
+            "id": "cdbb2ce20eaef521",
+            "topic": "④主要粮食作物播种面积遥感测量",
+            "expanded": true,
+            "parentid": "root",
+            "direction": "left",
+            "note": ""
+        },
+        {
+            "id": "cdbd376add340942",
+            "topic": "数据准备",
+            "expanded": true,
+            "parentid": "cdbb2ce20eaef521",
+            "note": ""
+        },
+        {
+            "id": "cdbd8731dea0e20e",
+            "topic": "多源汇聚",
+            "expanded": true,
+            "parentid": "cdbd376add340942",
+            "note": ""
+        },
+        {
+            "id": "cdbe1043c09b9586",
+            "topic": "高分影像",
+            "expanded": true,
+            "parentid": "cdbd8731dea0e20e",
+            "note": ""
+        },
+        {
+            "id": "cdbde1975be0c8cc",
+            "topic": "选用作物关键期影像，空间分辨率优于2(含) 米",
+            "expanded": true,
+            "parentid": "cdbe1043c09b9586",
+            "note": ""
+        },
+        {
+            "id": "cdbe13165811d71d",
+            "topic": "已有数据",
+            "expanded": true,
+            "parentid": "cdbd8731dea0e20e",
+            "note": ""
+        },
+        {
+            "id": "cdbe17d7cf37d470",
+            "topic": "县界矢量、物候信息、历史数据",
+            "expanded": true,
+            "parentid": "cdbe13165811d71d",
+            "note": ""
+        },
+        {
+            "id": "cdbe2312c12f7e74",
+            "topic": "上游数据",
+            "expanded": true,
+            "parentid": "cdbd8731dea0e20e",
+            "note": ""
+        },
+        {
+            "id": "cdbe2644c5263a7e",
+            "topic": "实地样方、种植用地底图",
+            "expanded": true,
+            "parentid": "cdbe2312c12f7e74",
+            "note": ""
+        },
+        {
+            "id": "cdbd3799dd700e88",
+            "topic": "内业测量",
+            "expanded": true,
+            "parentid": "cdbb2ce20eaef521",
+            "note": ""
+        },
+        {
+            "id": "cdbe02b4dec342de",
+            "topic": "技术路径",
+            "expanded": true,
+            "parentid": "cdbd3799dd700e88",
+            "note": ""
+        },
+        {
+            "id": "cdbded6cd70219be",
+            "topic": "首选方案",
+            "expanded": true,
+            "parentid": "cdbe02b4dec342de",
+            "note": ""
+        },
+        {
+            "id": "cdbdf823e5d7662d",
+            "topic": "AI 深度学习（高分影像分类模型）+ 人工修正",
+            "expanded": true,
+            "parentid": "cdbded6cd70219be",
+            "note": ""
+        },
+        {
+            "id": "cdbded77c4283d91",
+            "topic": "备选方案",
+            "expanded": true,
+            "parentid": "cdbe02b4dec342de",
+            "note": ""
+        },
+        {
+            "id": "cdbdfaf54d6f42ef",
+            "topic": "传统机器学习（随机森林、支持向量机等）+ 人工修正",
+            "expanded": true,
+            "parentid": "cdbded77c4283d91",
+            "note": ""
+        },
+        {
+            "id": "cdbe068542530241",
+            "topic": "产出成果",
+            "expanded": true,
+            "parentid": "cdbd3799dd700e88",
+            "note": ""
+        },
+        {
+            "id": "cdbe09b7474c0896",
+            "topic": "分品类播种面积及空间分布图",
+            "expanded": true,
+            "parentid": "cdbe068542530241",
+            "note": ""
+        },
+        {
+            "id": "cdbd37a5d2d5511c",
+            "topic": "质量检查",
+            "expanded": true,
+            "parentid": "cdbb2ce20eaef521",
+            "note": ""
+        },
+        {
+            "id": "cdbe3087ce75731b",
+            "topic": "自检+三方",
+            "expanded": true,
+            "parentid": "cdbd37a5d2d5511c",
+            "note": ""
+        },
+        {
+            "id": "cdbb2dc0f4932d92",
+            "topic": "⑤大宗经济作物播种面积遥感测量",
+            "expanded": true,
+            "parentid": "root",
+            "direction": "left",
+            "note": ""
+        },
+        {
+            "id": "cdbe3359c9efee61",
+            "topic": "数据准备",
+            "expanded": true,
+            "parentid": "cdbb2dc0f4932d92",
+            "note": ""
+        },
+        {
+            "id": "cdbef8adb842e8f1",
+            "topic": "与④一起",
+            "expanded": true,
+            "parentid": "cdbe3359c9efee61",
+            "note": ""
+        },
+        {
+            "id": "cdbe336a430df911",
+            "topic": "内业测量",
+            "expanded": true,
+            "parentid": "cdbb2dc0f4932d92",
+            "note": ""
+        },
+        {
+            "id": "cdbf230ba8526fbc",
+            "topic": "与④一起",
+            "expanded": true,
+            "parentid": "cdbe336a430df911",
+            "note": ""
+        },
+        {
+            "id": "cdbe3378424f6d55",
+            "topic": "质量检查",
+            "expanded": true,
+            "parentid": "cdbb2dc0f4932d92",
+            "note": ""
+        },
+        {
+            "id": "cdbf24004b79be46",
+            "topic": "与④一起",
+            "expanded": true,
+            "parentid": "cdbe3378424f6d55",
+            "note": ""
+        },
+        {
+            "id": "cdbb318b0bd88314",
+            "topic": "③设施农业遥感测量",
+            "expanded": true,
+            "parentid": "root",
+            "direction": "right",
+            "note": ""
+        },
+        {
+            "id": "cdbd082568b5f9bf",
+            "topic": "数据准备",
+            "expanded": true,
+            "parentid": "cdbb318b0bd88314",
+            "note": ""
+        },
+        {
+            "id": "cdbd16fcd0e242c0",
+            "topic": "与②一起",
+            "expanded": true,
+            "parentid": "cdbd082568b5f9bf",
+            "note": ""
+        },
+        {
+            "id": "cdbd08381a13b79b",
+            "topic": "种植设施识别",
+            "expanded": true,
+            "parentid": "cdbb318b0bd88314",
+            "note": ""
+        },
+        {
+            "id": "cdbd1e535bc8e24d",
+            "topic": "基于温室/大棚影像特征，提取设施地块",
+            "expanded": true,
+            "parentid": "cdbd08381a13b79b",
+            "note": ""
+        },
+        {
+            "id": "cdbd2107574e2411",
+            "topic": "畜禽设施提取",
+            "expanded": true,
+            "parentid": "cdbb318b0bd88314",
+            "note": ""
+        },
+        {
+            "id": "cdbd238d57bbd028",
+            "topic": "实地信息 + 影像特征；聚焦切割点以上养殖场（户）",
+            "expanded": true,
+            "parentid": "cdbd2107574e2411",
+            "note": ""
+        },
+        {
+            "id": "cdbd2615e5e69516",
+            "topic": "质量检查",
+            "expanded": true,
+            "parentid": "cdbb318b0bd88314",
+            "note": ""
+        },
+        {
+            "id": "cdbd29565428655c",
+            "topic": "与②一起",
+            "expanded": true,
+            "parentid": "cdbd2615e5e69516",
+            "note": ""
+        },
+        {
+            "id": "cdbb348e043cad7a",
+            "topic": "⑥遥感测量结果数据核实",
+            "expanded": true,
+            "parentid": "root",
+            "direction": "left",
+            "note": ""
+        },
+        {
+            "id": "cdbf3c363cbc39ea",
+            "topic": "人工目视核实",
+            "expanded": true,
+            "parentid": "cdbb348e043cad7a",
+            "note": ""
+        },
+        {
+            "id": "cdbf6d14260c65d2",
+            "topic": "省级专班集中核实",
+            "expanded": true,
+            "parentid": "cdbf3c363cbc39ea",
+            "note": ""
+        },
+        {
+            "id": "cdbf3c49a836a74d",
+            "topic": "现场核实",
+            "expanded": true,
+            "parentid": "cdbb348e043cad7a",
+            "note": ""
+        },
+        {
+            "id": "cdbf72cab46282ca",
+            "topic": "按县核实",
+            "expanded": true,
+            "parentid": "cdbf3c49a836a74d",
+            "note": ""
+        },
+        {
+            "id": "cdbf3c57387112c5",
+            "topic": "结果修订",
+            "expanded": true,
+            "parentid": "cdbb348e043cad7a",
+            "note": ""
+        }
     ]
 };
